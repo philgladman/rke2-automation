9a905c707bd306c10c46a7d7926510aed00f55bd
Phillip Gladman <Philgladman@Phillps-MacBook-Pro.local>
2022-09-01 14:45:31 -0400

    updates to use absolute file locations

--- create-ec2-instances/command-create-stack.sh
+++ create-ec2-instances/command-create-stack.sh
@@ -12,7 +12,7 @@ fi
 export STACK_NAME=$1
 
 ### create cloudformation  stack
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://~/rke2-automation/create-cluster.yml --region us-east-1
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://~/rke2-automation/create-ec2-instances/create-cluster.yml --region us-east-1
 
 ### show progress bar that takes 30 seconds
 {
@@ -26,27 +26,27 @@ aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://
 }
 
 ### check to see if outputs are available and put into file
-aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > tmp-script-output.json
+aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > ~/rke2-automation/create-ec2-instances/tmp-script-output.json
 
 ### check to see if outputs file is empty, if so then then run loop until there are outputs and put in file
 while true # infinite loop
 do
-    output=$( cat tmp-script-output.json )
+    output=$( cat ~/rke2-automation/create-ec2-instances/script-output.json )
     if [ -z "$output" ]
     then
         # output is empty - failure - rerun aws command:
         echo "cloudformation stack outputs are empty, waiting for stack to complete..."
-        aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > tmp-script-output.json
+        aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > ~/rke2-automation/create-ec2-instances/tmp-script-output.json
     else
         # file has output - success - leave the loop:
         echo "cloudformation stack creation complete"
-        aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > script-output.json
-        rm tmp-script-output.json
+        aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[].Outputs[].[OutputKey,OutputValue]" --region us-east-1 --output text > ~/rke2-automation/create-ec2-instances/script-output.json
+        rm ~/rke2-automation/create-ec2-instances/script-output.json
         break
     fi
 done
 
 ### run python script to take output file and format to ansible variables file
-python3 script-1.py
+python3 ~/rke2-automation/create-ec2-instances/script-1.py
 
-rm -f script-output.json
+rm -f ~/rke2-automation/create-ec2-instances/script-output.json
